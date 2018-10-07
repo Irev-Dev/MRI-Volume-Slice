f5dc37f0df99536bc672efa6895027c75c6dec04
Charlie, IrevDev <irevdev@protonmail.ch>
2018-10-07 03:09:56 +0000

    Tidy code a little

--- script.js
+++ script.js
@@ -3,24 +3,21 @@ import MRISlice from './MRI-Volume-Slice';
 
 const mRISlice = new MRISlice();
 
-function setUp3ViewPorts() {
+const xDiv = document.getElementById('z-view-wrapper');
+const yDiv = document.getElementById('y-view-wrapper');
+const zDiv = document.getElementById('x-view-wrapper');
 
-    const xDiv = document.getElementById('z-view-wrapper');
+function appendCanvasesToHTML() {
     xDiv.appendChild(mRISlice.canvases.z);
-
-    const yDiv = document.getElementById('y-view-wrapper');
     yDiv.appendChild(mRISlice.canvases.y);
-
-    const zDiv = document.getElementById('x-view-wrapper');
     zDiv.appendChild(mRISlice.canvases.x);
-
 }
 
 document.getElementById('file-input').onchange = function (event) {
     const fr = new FileReader();
     fr.onload = () => {
         mRISlice.loadNewNifti(nifti.parse(fr.result));
-        setUp3ViewPorts();
+        appendCanvasesToHTML();
         mRISlice.mouseNavigationEnabled('enable please')
     }
     fr.readAsArrayBuffer(event.target.files[0]);
