10ea3316efa1bd073ad604fd2b3972970aa1eec7
Kurt Charlie Hutten <kurt.hutten@accelo.com>
2019-10-16 03:09:16 +1100

    An attempt at fixing ff issues

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 import 'babel-polyfill';
 import nifti from 'nifti-js';
 import pako from 'pako';
-import * as idb from 'idb-keyval';
+import { get as idbGet, set as idbSet } from 'idb-keyval';
 import MRISlice from './mri-volume-slice';
 
 const mRISlice = new MRISlice();
 
+const LAST_NIFTI_KEY = 'Last-nifti-file';
 const xDiv = document.getElementById('z-view-wrapper');
 const yDiv = document.getElementById('y-view-wrapper');
 const zDiv = document.getElementById('x-view-wrapper');
@@ -37,7 +38,7 @@ document.getElementById('toggle-cross-hairs').onchange = (event) => {
 };
 
 function setupNifti(file) {
-  idb.set('LastNiftiFile', file);
+  idbSet(LAST_NIFTI_KEY, file);
   mRISlice.loadNewNifti(nifti.parse(file));
   mRISlice.mouseNavigationEnabled('enable please');
   hideLoader();
@@ -47,25 +48,41 @@ function hideLoader() {
   const loader = document.getElementById('principal-loader');
   loader.style.display = 'none';
 }
+function showError() {
+  hideLoader();
+  const loader = document.getElementById('error');
+  loader.style.display = 'block';
+}
 
 
 async function loadDefaultData() {
-  const lastFile = await idb.get('LastNiftiFile');
+  let lastFile;
+  try {
+    lastFile = await idbGet(LAST_NIFTI_KEY);
+  } catch (e) {
+    console.error(e);
+  }
+  console.log('fetching');
   if (lastFile) return setupNifti(lastFile);
   const url = 'https://openneuro.org/crn/datasets/ds001417/files/sub-study002:ses-after:anat:sub-study002_ses-after_T1w.nii.gz';
   // load from the cache API or fetch if not found
   let response;
   if ('caches' in window) {
-    response = await caches.match(url);
-    if (!response) {
-      response = await fetch(url);
-      const cache = await caches.open(url);
-      await cache.put(url, response.clone());
+    try {
+      response = await caches.match(url);
+      if (!response) {
+        response = await fetch(url);
+        const cache = await caches.open(url);
+        await cache.put(url, response.clone());
+      }
+    } catch (e) {
+      console.error(e);
     }
   } else {
     // browser does not support the cache APi
     response = await fetch(url);
   }
+  if (!response) showError();
 
   const blob = await response.arrayBuffer();
   const compressed = new Uint8Array(blob);
